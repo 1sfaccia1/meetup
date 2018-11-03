f0c341d77f162ee026e58abee524b550c955bff9
1stfaccia1 <chad.m.wittman75@gmail.com>
2018-11-03 09:59:44 -0500

    added

---
+++ client_src/src/components/Meetup.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Meetup extends Component {
+  render(){
+    return(
+      <div>
+        <h1>Meetups</div>
+      </div>
+    )
+  }
+}
+
+export default Meetups;
