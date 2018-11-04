ccafd2f0e733b0f00af57f3a9495130c5150da01
1stfaccia1 <chad.m.wittman75@gmail.com>
2018-11-04 10:31:55 -0600

    added

--- client_src/src/App.js
+++ client_src/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Main from './components/Main';
 import Navbar from './components/Navbar';
+import { Link } from 'react-router-dom';
 
 
 
@@ -10,6 +11,10 @@ const App = () => (
     <Navbar/>
     <div className="container">
       <Main />
+    </div>
+    <div className="fixed-action-btn">
+      <Link to="/meetups/add" className="btn-floating btn-large red"><i className="fa fa-plus"></i>
+      </Link>
     </div>
   </div>
 )
--- client_src/src/components/Navbar.js
+++ client_src/src/components/Navbar.js
@@ -1,29 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
   render(){
     return(
       <div>
-      <nav class="nav-extended">
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo">Logo</a>
-      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
-    </div>
-    <div class="nav-content">
-      <ul class="tabs tabs-transparent">
-        <li class="tab"><a href="#test1">Test 1</a></li>
-        <li class="tab"><a class="active" href="#test2">Test 2</a></li>
-        <li class="tab disabled"><a href="#test3">Disabled Tab</a></li>
-        <li class="tab"><a href="#test4">Test 4</a></li>
-      </ul>
-    </div>
-  </nav>
-
+        <nav className="blue darken-3">
+          <div className="nav-wrapper">
+            <a href="/" className="brand-logo center">Meetups</a>
+            <a href="sidenav" data-activates="main-menu" className="button-collapse show-on-large"><i className="fa fa-bars"></i></a>
+            <ul className="right hide-on-sm-only">
+              <li><Link to = "/"><i className="fa fa-users"></i>Meetups</Link></li>
+            </ul>
+            <ul id="main-menu slide-out" className="sidenav">
+              <li><Link to = "/"><i className="fa fa-users"></i>Meetups</Link></li>
+              <li><Link to = "/meetups/add"><i className="fa fa-plus"></i>Add Meetups</Link></li>
+              <li><Link to = "/about"><i className="fa fa-question-circle"></i>About</Link></li>
+            </ul>
+          </div>
+        </nav>
       </div>
     )
   }
